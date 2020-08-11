da904233f3b86fcb68616e6929fda3c41c98eae2
domingo <ing.domingo.sl@growish.com>
2020-08-11 13:30:15 +0200

    General fix

--- Brightmansukhsoft/Scuolapayment/Controller/Redirect/Scuolapayredirect.php
+++ Brightmansukhsoft/Scuolapayment/Controller/Redirect/Scuolapayredirect.php
@@ -16,19 +16,12 @@ class Scuolapayredirect extends \Magento\Framework\App\Action\Action
     protected $orderRepository;
     protected $customerSession;
     private $timezone;
+    protected $_encryptor;
     protected $_resource;
     protected $helper;
     protected $_coreRegistry = null;
-    public function __construct(Context $context, PageFactory $pageFactory,
-	\Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig, 
-	\Brightmansukhsoft\Scuolapayment\Helper\Data $helperData,
-	\Magento\Framework\App\ResourceConnection $resource,
-	\Magento\Store\Model\StoreManagerInterface $storemanager,
-	\Magento\Checkout\Model\Session $checkoutSession, 
-	\Magento\Sales\Api\OrderRepositoryInterface $orderRepository,
-	\Magento\Customer\Model\Session $customerSession, 
-	\Magento\Framework\Registry $registry,
-	\Magento\Framework\Stdlib\DateTime\TimezoneInterface $timezone) {
+    public function __construct(Context $context, PageFactory $pageFactory, \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig, \Brightmansukhsoft\Scuolapayment\Helper\Data $helperData, \Magento\Framework\App\ResourceConnection $resource, \Magento\Framework\Encryption\EncryptorInterface $encryptor, \Magento\Store\Model\StoreManagerInterface $storemanager, \Magento\Checkout\Model\Session $checkoutSession, \Magento\Sales\Api\OrderRepositoryInterface $orderRepository, \Magento\Customer\Model\Session $customerSession, \Magento\Framework\Registry $registry, \Magento\Framework\Stdlib\DateTime\TimezoneInterface $timezone)
+    {
         $this->pageFactory = $pageFactory;
         $this->scopeConfig = $scopeConfig;
         $this->helper = $helperData;
@@ -38,88 +31,113 @@ class Scuolapayredirect extends \Magento\Framework\App\Action\Action
         $this->orderRepository = $orderRepository;
         $this->customerSession = $customerSession;
         $this->timezone = $timezone;
-		$this->_coreRegistry     = $registry;
+        $this->_encryptor = $encryptor;
+        $this->_coreRegistry = $registry;
 
         parent::__construct($context);
     }
-	public function getStoreCurrency() {
-        $currency_code = $this->storemanager->getStore()->getCurrentCurrency()->getCode();
-        $currency_array = array(
-            'MYR' => '458','PLN' => '985','NOK' => '578','RUB' => '643','AED' => '784','CNY' => '156','KRW' => '410','ILS' => '376','SAR' => '682','TRY' => '949',
-            'HKD' => '344','KWD' => '414','INR' => '356','RON' => '946','SGD' => '702','MXN' => '484','NZD' => '554','EEK' => '233','LTL' => '440','USD' => '840',
-            'ZAR' => '710','CAD' => '124','JPY' => '392','SEK' => '752','CZK' => '203','DKK' => '208','EUR' => '978','GBP' => '826','CHF' => '756','HRK' => '191',
-            'BHD' => '048','HUF' => '348','AUD' => '036','BRL' => '986','NGN' => '566','QAR' => '634','THB' => '764','BND' => '096','MAD' => '504','TND' => '788',
-            'BIF' => '108','BYN' => '933','ARS' => '032','CLP' => '152','BDT' => '050','KES' => '404','MUR' => '480','NPR' => '524','BYR' => '974','MDL' => '498',
-            'BOB' => '068','PYG' => '600','NAD' => '516','DZD' => '012','BBD' => '052','OMR' => '512','BSD' => '044','BZD' => '084','KYD' => '136','DOP' => '214',
-            'GYD' => '328','JMD' => '388','ANG' => '532','AWG' => '533','TTD' => '780','XCD' => '951','SRD' => '968','UGX' => '800','MMK' => '104','UYU' => '858',
-            'COP' => '170','EGP' => '818','FJD' => '242','IDR' => '360','IQD' => '368','IRR' => '364','ISK' => '352','LAK' => '418','LKR' => '144','MOP' => '446',
-            'PHP' => '608','PKR' => '586','SCR' => '690','TWD' => '901','VND' => '704','PEN' => '604','RSD' => '941','KZT' => '398','BWP' => '072','BGN' => '975',
-            'AZN' => '944','UAH' => '980','LBP' => '422','TZS' => '834','BMD' => '060','JOD' => '400','AFN' => '971','CRC' => '188','XOF' => '952','GTQ' => '320',
-        );
 
-        return isset($currency_array[$currency_code]) ? $currency_array[$currency_code] : "";
-    }
-	public function execute()
+    public function execute()
     {
-		$resultPage = $this->pageFactory->create();
+        $resultPage = $this
+            ->pageFactory
+            ->create();
         $storeScope = \Magento\Store\Model\ScopeInterface::SCOPE_STORE;
-        // Order details
-       //$orderNoData=$this->getOrderno();
-       //$orderId=$orderNoData[0]['entity_id'];
-	   $orderId = $this->_checkoutSession->getLastRealOrder()->getId();
-	 	if($orderId){
-	 	      $orderNo =$this->_checkoutSession->getLastRealOrder()->getIncrementId();
-              $order = $this->_objectManager->create('Magento\Sales\Model\Order')->load($orderId);
-              $total_amt = number_format($order->getGrandTotal(), 2, '.', '');
-              $amt=$total_amt*100;
-              $total_amt= number_format($amt, 2,'.', '');
-              
-	 	    /* Api Keys Secret, Bussiness Key, Url */
-	 	    
-	 	   $helper = $this->helper;
-            $environment=$helper->getConfig('payment/scuolapayment/environment');
-            if($environment=='Production'){
-                  $redirectUrl='https://webpayments-api.scuolapay.it';
-                  $webhook=$helper->getConfig('payment/scuolapaymentsection/scuolapayment/production_webhook');
-                  $secret=$helper->getConfig('payment/scuolapaymentsection/scuolapayment/production_secret');
-                  $business=$helper->getConfig('payment/scuolapaymentsection/scuolapayment/production_bussiness');
-                  
-            } else {
-                    $redirectUrl='https://webpayments-api-dev.scuolapay.it';
-                    $webhook=$helper->getConfig('payment/scuolapaymentsection/scuolapayment/integrate_webhook');
-                    $secret=$helper->getConfig('payment/scuolapaymentsection/scuolapayment/integrate_secret');
-                    $business=$helper->getConfig('payment/scuolapaymentsection/scuolapayment/integrate_bussiness');
+        $orderId = $this
+            ->_checkoutSession
+            ->getLastRealOrder()
+            ->getId();
+        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+        $currencysymbol = $objectManager->get('Magento\Store\Model\StoreManagerInterface');
+        $currency = $currencysymbol->getStore()
+            ->getCurrentCurrencyCode();
+        if ($currency == 'EUR')
+        {
+            if ($orderId)
+            {
+                $orderNo = $this
+                    ->_checkoutSession
+                    ->getLastRealOrder()
+                    ->getIncrementId();
+                $order = $this
+                    ->_objectManager
+                    ->create('Magento\Sales\Model\Order')
+                    ->load($orderId);
+                $total = $order->getGrandTotal();
+                $total_amt = $total + 0;
+                $total_amt = number_format($total_amt, 2, '.', '');
+                $total_amt = $total_amt * 100;
+                $total_amt = (string)$total_amt;
+
+                $helper = $this->helper;
+                $environment = $helper->getConfig('payment/scuolapayment/environment');
+                if ($environment == 'Production')
+                {
+                    $redirectUrl = 'https://webpayments-api.scuolapay.it';
+                    $webhook = $helper->getConfig('payment/scuolapaymentsection/scuolapayment/production_webhook');
+                    $secretEncrypted = $helper->getConfig('payment/scuolapaymentsection/scuolapayment/integrate_secret');
+                    $secret = $this
+                        ->_encryptor
+                        ->decrypt($secretEncrypted);
+                    $business = $helper->getConfig('payment/scuolapaymentsection/scuolapayment/production_bussiness');
+
+                }
+                else
+                {
+                    $redirectUrl = 'https://webpayments-api-dev.scuolapay.it';
+                    $webhook = $helper->getConfig('payment/scuolapaymentsection/scuolapayment/integrate_webhook');
+                    $secretEncrypted = $helper->getConfig('payment/scuolapaymentsection/scuolapayment/integrate_secret');
+                    $secret = $this
+                        ->_encryptor
+                        ->decrypt($secretEncrypted);
+                    $business = $helper->getConfig('payment/scuolapaymentsection/scuolapayment/integrate_bussiness');
+                }
+                $responseUrl = $this
+                    ->storemanager
+                    ->getStore()
+                    ->getBaseUrl() . 'scuolapay/response';
+                if ($webhook)
+                {
+                    $orderData = array(
+                        'secret' => $secret,
+                        'business' => $business,
+                        'redirectUrl' => $redirectUrl,
+                        'amount' => $total_amt,
+                        'orderId' => $orderNo,
+                        'responseURL' => $responseUrl,
+                        'webhookURL' => $webhook
+                    );
+                }
+                else
+                {
+                    $orderData = array(
+                        'secret' => $secret,
+                        'business' => $business,
+                        'redirectUrl' => $redirectUrl,
+                        'amount' => $total_amt,
+                        'orderId' => $orderNo,
+                        'responseURL' => $responseUrl,
+                        'webhookURL' => ''
+                    );
+                }
+                $this
+                    ->_coreRegistry
+                    ->register('orderdata', $orderData);
+                $page = $this
+                    ->pageFactory
+                    ->create();
+                return $page;
             }
-             // $secret = "thisisaverybadsecret";
-              $responseUrl= $this->storemanager->getStore()->getBaseUrl().'scuolapay/response';
-              if($webhook){
-	          $orderData=array(
-	                               'secret'=> $secret,
-	                               'business'=> $business,
-	                               'redirectUrl'=> $redirectUrl,
-	                               'amount' =>$total_amt,
-	                               'orderId' =>  $orderNo,
-	                               'responseURL' => $responseUrl,
-                                   'webhookURL' => $webhook
-	                            ); 
-              } else {
-                                    	          $orderData=array(
-	                               'secret'=> $secret,
-	                               'business'=> $business,
-	                               'redirectUrl'=> $redirectUrl,
-	                               'amount' =>$total_amt,
-	                               'orderId' =>  $orderNo,
-	                               'responseURL' => $responseUrl,
-                                   'webhookURL' => ''
-	                            );
-              }
-	     	  $this->_coreRegistry->register('orderdata', $orderData);
-		       $page = $this->pageFactory->create();
-             return $page;
-        } else {
-            die('.......');
-            $this->getResponse()->setRedirect($this->getUrl());
-       }
-     
-	}
+            else
+            {
+                echo 'Order id is not found.';
+            }
+        }
+        else
+        {
+            echo "Only EUR currency accepted";
+        }
+
+    }
 }
+
--- Brightmansukhsoft/Scuolapayment/Controller/Scuolaresponse/Response.php
+++ Brightmansukhsoft/Scuolapayment/Controller/Scuolaresponse/Response.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace Brightmansukhsoft\Scuolapayment\Controller\Scuolaresponse;
-
 use Magento\Framework\View\Result\PageFactory;
 use Magento\Framework\App\Action\Context;
 use Magento\Checkout\Model\Session;
@@ -12,10 +10,8 @@ use Magento\Framework\App\Response\Http;
 use Magento\Sales\Model\Order\Payment\Transaction\Builder as TransactionBuilder;
 use \Magento\CatalogInventory\Api\StockRegistryInterface;
 use Magento\Framework\App\RequestInterface;
-//use Magento\Framework\Controller\ResultFactory; 
 
 class Response extends \Magento\Framework\App\Action\Action
-
 {
     protected $_objectmanager;
     protected $_checkoutSession;
@@ -33,141 +29,99 @@ class Response extends \Magento\Framework\App\Action\Action
     protected $_invoiceService;
     protected $helper;
     protected $_storeManager;
-    public function __construct(Context $context,
-        Session $checkoutSession,RequestInterface $request, \Magento\Store\Model\StoreManagerInterface $storeManager, OrderFactory $orderFactory, ScopeConfigInterface $scopeConfig, Http $response, TransactionBuilder $tb,\Brightmansukhsoft\Scuolapayment\Helper\Data $helperData, \Magento\Checkout\Model\Cart $cart, \Magento\AdminNotification\Model\Inbox $inbox, \Magento\Sales\Api\TransactionRepositoryInterface $transactionRepository, StockRegistryInterface $stockRegistry,\Magento\Sales\Model\Service\InvoiceService $invoiceService) {
-        $this->_storeManager = $storeManager;
-        $this->checkoutSession = $checkoutSession;
-        $this->orderFactory = $orderFactory;
-        $this->response = $response;
-        $this->scopeConfig = $scopeConfig;
-        $this->transactionBuilder = $tb;
-        $this->cart = $cart;
-        $this->inbox = $inbox;
-        $this->helper = $helperData;
+    public function __construct(Context $context, Session $checkoutSession, RequestInterface $request, \Magento\Store\Model\StoreManagerInterface $storeManager, OrderFactory $orderFactory, ScopeConfigInterface $scopeConfig, Http $response, TransactionBuilder $tb, \Brightmansukhsoft\Scuolapayment\Helper\Data $helperData, \Magento\Checkout\Model\Cart $cart, \Magento\AdminNotification\Model\Inbox $inbox, \Magento\Sales\Api\TransactionRepositoryInterface $transactionRepository, StockRegistryInterface $stockRegistry, \Magento\Sales\Model\Service\InvoiceService $invoiceService)
+    {
+        $this->_storeManager         = $storeManager;
+        $this->checkoutSession       = $checkoutSession;
+        $this->orderFactory          = $orderFactory;
+        $this->response              = $response;
+        $this->scopeConfig           = $scopeConfig;
+        $this->transactionBuilder    = $tb;
+        $this->cart                  = $cart;
+        $this->inbox                 = $inbox;
+        $this->helper                = $helperData;
         $this->transactionRepository = $transactionRepository;
-        $this->urlBuilder = \Magento\Framework\App\ObjectManager::getInstance()
-                ->get('Magento\Framework\UrlInterface');
-        $this->stockRegistry = $stockRegistry;
-        $this->_invoiceService = $invoiceService;
-        $this->request = $request;
-
+        $this->urlBuilder            = \Magento\Framework\App\ObjectManager::getInstance()->get('Magento\Framework\UrlInterface');
+        $this->stockRegistry         = $stockRegistry;
+        $this->_invoiceService       = $invoiceService;
+        $this->request               = $request;
         parent::__construct($context);
     }
-	public function execute()
-
+    public function execute()
     {
-
-       $sessionId=$this->request->getParam('sessionId');
-       $helper = $this->helper;
-       $environment=$helper->getConfig('payment/scuolapayment/environment');
-       if($environment=='Production'){
-         $scuolaPayreponseurl='https://webpayments-api.scuolapay.it/session/'.$sessionId;  
+        $sessionId   = $this->request->getParam('sessionId');
+        $helper      = $this->helper;
+        $environment = $helper->getConfig('payment/scuolapayment/environment');
+        if ($environment == 'Production') {
+            $scuolaPayreponseurl = 'https://webpayments-api.scuolapay.it/session/' . $sessionId;
         } else {
-         $scuolaPayreponseurl='https://webpayments-api-dev.scuolapay.it/session/'.$sessionId;       
+            $scuolaPayreponseurl = 'https://webpayments-api-dev.scuolapay.it/session/' . $sessionId;
         }
-       $json = file_get_contents($scuolaPayreponseurl);
-       $obj = json_decode($json,true);
-        $status=$obj['status'];
-        $amt=$obj['amount'];
-        $orderId=$obj['orderId'];
-       
+        $arrContextOptions = array(
+            "ssl" => array(
+                "verify_peer" => false,
+                "verify_peer_name" => false
+            )
+        );
+        $json              = file_get_contents($scuolaPayreponseurl, false, stream_context_create($arrContextOptions));
+        $obj               = json_decode($json, true);
+        $status            = $obj['status'];
+        $amt               = $obj['amount'];
+        $orderId           = $obj['orderId'];
         if ($sessionId) {
-            $isnotify = true;
-            // Order ID
-            $orderid =$orderId;
-            # get order object
-            $order = $this->orderFactory->create()->loadByIncrementId($orderid);
-			$orderEmail=$order->getCustomerEmail();
-            $order_status = $order->getStatus(); 
-            
-            if (preg_match("/^$order_status/i", Order::STATE_PENDING_PAYMENT)){
-                if ($sessionId) {
-                    $approval_code = $status;
-                    if ($approval_code == '1') {
-                        // Needed response values                    
-                        //$amount = $response['chargetotal'];
-                        $order->setStatus(Order::STATE_PROCESSING, true);
-                        //$order->setTotalPaid($amount);
-                        $order->setTotalDue(0);
-                        $order->save();
-                        //Store details based on IPG response
-                        //$this->storeData($response);
-						$objectManager = \Magento\Framework\App\ObjectManager::getInstance();
-						$order->setCustomerEmail($orderEmail);
-						$objectManager->create('Magento\Sales\Model\OrderNotifier')->notify($order);
-						//die('success1');
-                        //$this->_redirect($this->urlBuilder->getUrl('checkout/onepage/success', array('_secure' => true)));
-                       // $resultRedirect->setUrl('checkout/onepage/success');
-                             $baseUrl= $this->_storeManager->getStore()->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_WEB);
-                       //$resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
-                      // $this->messageManager->addSuccess($result['message']);
-				    //	$this->checkoutSession->clearQuote();
-                      // $this->_redirect('checkout/onepage/success');
-                       header("Location:".$baseUrl.'checkout/onepage/success');
-                    } else if ($approval_code == '0') {
-                        $order->setState(Order::STATE_CANCELED, true);
-                        // Inventory updated 
-                        $this->updateInventory($orderid);
-                        $order->cancel()->save();
-                        //die('fail');
-                       // $this->checkoutSession->clearQuote();
-                        header("Location:".$baseUrl.'checkout/onepage/failure');
-                        //$resultRedirect->setUrl('checkout/onepage/failure');
-                        //return $resultRedirect;
-                        //$this->_redirect($this->urlBuilder->getUrl('checkout/onepage/failure', array('_secure' => true)));
-                    } else {
-                        $order->setState(Order::STATE_CANCELED, true);
-                        // Inventory updated                 
-                        $this->updateInventory($orderid);
-                        $order->cancel()->save();
-						//die('fail');
-									//		$this->checkoutSession->clearQuote();
-						 header("Location:".$baseUrl.'checkout/onepage/failure');
-						//$resultRedirect->setUrl('checkout/onepage/failure');
-						 //return $resultRedirect;
-                        //$this->_redirect($this->urlBuilder->getUrl('checkout/onepage/failure', array('_secure' => true)));
-                    }
-                } else {
-                    
+            $isnotify      = true;
+            $orderid       = $orderId;
+            $order         = $this->orderFactory->create()->loadByIncrementId($orderid);
+            $orderEmail    = $order->getCustomerEmail();
+            $order_status  = $order->getStatus();
+            $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+            $storeManager  = $objectManager->get('\Magento\Store\Model\StoreManagerInterface');
+            $store         = $storeManager->getStore();
+            $baseUrl       = $store->getBaseUrl();            
+            if (preg_match("/^$order_status/i", Order::STATE_PENDING_PAYMENT)) {
+                $approval_code = $status;
+                if ($approval_code == '1') {
+                    $order->setStatus(Order::STATE_PROCESSING, true);
+                    $order->setTotalDue(0);
+                    $order->save();
+                    $order->setCustomerEmail($orderEmail);
+                    $objectManager->create('Magento\Sales\Model\OrderNotifier')->notify($order);
+                    header("Location:" . $baseUrl . 'checkout/onepage/success', true, 301);
+                    exit;
+                } else if ($approval_code == '0') {
                     $order->setState(Order::STATE_CANCELED, true);
-                    // Inventory updated 
                     $this->updateInventory($orderid);
                     $order->cancel()->save();
-                   // $this->checkoutSession->clearQuote();
-					header("Location:".$baseUrl.'checkout/onepage/failure');
-					//$resultRedirect->setUrl('checkout/onepage/failure');
-				    //return $resultRedirect;
-                   //$this->_redirect($this->urlBuilder->getUrl('checkout/onepage/failure', array('_secure' => true)));
+                    header("Location:" . $baseUrl . 'checkout/onepage/failure', true, 301);
+                    exit;
+                } else {
+                    $order->setState(Order::STATE_CANCELED, true);
+                    $this->updateInventory($orderid);
+                    $order->cancel()->save();
+                    header("Location:" . $baseUrl . 'checkout/onepage/failure', true, 301);
+                    exit;
                 }
-            } else{
-				die('Already ScuolaPay response updated for order');
+            } else {
+                echo 'Already ScuolaPay response updated for order';
             }
+        } else {
+            $order->setState(Order::STATE_CANCELED, true);
+            $this->updateInventory($orderid);
+            $order->cancel()->save();
+            header("Location:" . $baseUrl . 'checkout/onepage/failure', true, 301);
+            exit;
         }
-       
-
-     
-
-	}
-	 public function getCustomerId() {
-        //return current customer ID
-        return $this->_checkoutSession->getId();
     }
-
-    public function updateInventory($orderId) {
-
-        # get order object
+    public function updateInventory($orderId)
+    {
         $order = $this->orderFactory->create()->loadByIncrementId($orderId);
         $items = $order->getAllItems();
         foreach ($items as $itemId => $item) {
             $ordered_quantity = $item->getQtyToInvoice();
-            $sku = $item->getSku();
-            $stockItem = $this->stockRegistry->getStockItemBySku($sku);
-            //$qtyStock = $stockItem->getQty();
-            //$this->logger->info("sku:".$sku.", qtyStock: ".$qtyStock.", ordered_quantity: ".$ordered_quantity);
-            //$updated_inventory = $qtyStock + $ordered_quantity;
+            $sku              = $item->getSku();
+            $stockItem        = $this->stockRegistry->getStockItemBySku($sku);
             $stockItem->setQtyCorrection($ordered_quantity);
             $this->stockRegistry->updateStockItemBySku($sku, $stockItem);
-        }		
+        }
     }
-
 }
